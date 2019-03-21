1ae7a75fe570dd2c87c2d97efcd7735fbd0f731a
Satnam89 <satnambrar90@gmail.com>
2019-03-21 17:11:58 -0400

    Setting class names dynamically

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ deletePersonHandler = (personIndex) =>{
   this.setState({persons: persons})
 }
 
+
 nameChangedHandler =(event, id)=> {
     const personIndex=this.state.persons.findIndex((p)=>{
     return p.id==id;
@@ -38,11 +39,22 @@ togglePersonsHandler= ()=>{
   const doesShow=this.state.showPersons;
   this.setState({showPersons:!doesShow});
 }
+
+
   render() {
+
+    let classes=[];
+    if(this.state.persons.length <=2){
+      classes.push('red');
+    }
+    if(this.state.persons.length <=1){
+      classes.push('bold');
+    }
+
     const style1={
-      backgrounColor:'Red',
+      backgroundColor:'green',
       font:'inherit',
-      border:'1px solid blue',
+      border:'1px solid green',
       padding:'8px',
       cursor:'pointer'
     }
@@ -61,12 +73,13 @@ togglePersonsHandler= ()=>{
                 changed={(event)=> this.nameChangedHandler(event, person.id)}/>
             })}
         </div> )
+        style1.backgroundColor='red';
     }
 
   return (
           <div className="App">
             <h1>Hi, i am a React App</h1>
-            <p>This is working fine</p>
+            <p className={classes.join(' ')}>This is working fine</p>
             <button 
               style={style1}
               onClick={this.togglePersonsHandler}> Toggle Persons </button>
@@ -79,64 +92,4 @@ togglePersonsHandler= ()=>{
 }
 
 export default App;
-//const app= props =>{
-  //   const [personState, setPersonState]=useState({
-  //     persons: [
-  //       { name: 'Satnam Singh', age: 25 },
-  //       { name:'Jatinder',age :23},
-  //       { name:'Maninder Singh',age :26}
-  //      ]
-  //     //  otherState:'someValue'
-  //     });
-  
-  // const [otherState,setOtherState]=useState('someOtherState');
-  // //console.log(personState,otherState);
-  
-  //     const switchNameHandler =(newName) =>{
-  //     setPersonState(
-  //       {
-  //         persons: [
-  //             { name: newName, age: 25 },
-  //             { name:'Maninder Singh',age : 30},
-  //             { name:'Mani Singh',age : 30}
-  //            ]
-  //            //otherState:pesonsState.otherState 
-  //       }
-  //     )
-  // }
-  
-  // const nameChangedHandler =(event)=> {
-  //   setPersonState(
-  //     {
-  //       persons: [
-  //           { name: 'Satnam', age: 25 },
-  //           { name:event.target.value,age : 30},
-  //           { name:'Mani Singh',age : 30}
-  //          ]
-  //     }
-  //   )
-  
-  // }
-  
-  //   return (
-  //           <div className="App">
-  //             <h1>Hi, i am a React App</h1>
-  //             <p>This is working fine</p>
-  //             <button onClick={()=>switchNameHandler.bind(this,'Satnam')}>Click!</button>
-  //             <Persons 
-  //               name={personState.persons[0].name} 
-  //               age={personState.persons[0].age}/>
-  //             <Persons 
-  //               name={personState.persons[1].name} 
-  //               age={personState.persons[1].age}
-  //               clicks ={switchNameHandler.bind(this,'SatnamBrar')}
-  //               changed={nameChangedHandler}>
-  //               My Hobbies: Rugby
-  //               </Persons>
-  //             <Persons 
-  //               name={personState.persons[2].name} 
-  //               age={personState.persons[2].age}/>
-  //           </div>
-           
-  //         );
-  // }
+
